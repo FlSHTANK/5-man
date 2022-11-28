3620fa69ca4d5308b043c859f58bc19f55084c15
FlSHTANK <119285193+FlSHTANK@users.noreply.github.com>
2022-11-28 16:51:52 +0700

    IDK

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿student student1 = new student();
+student1.name = "Natthakit";
+student1.weight = 80.5f;
+
+student student2 = new student();
+student2.name = "Teera";
+student2.weight = 60.7f;
+
+student student3 = new student();
+student3.name = "Phanu";
+student3.weight = 60.2f;
+
+student student4 = new student();
+student4.name = "Thanakon";
+student4.weight = 50.8f;
+
+student student5 = new student();
+student5.name = "Worathep";
+student5.weight = 100.9f;
+
+float AW = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5;
+Console.WriteLine(AW);
+Console.WriteLine("Thanakon 50.8");
+Console.WriteLine("Worathep 100.9");
+Console.WriteLine("Natthakit Mitsungnoen 653450283-5");
